5329d03c606010620bf4f396ae2a1321eadbd729
Nikolay Noskov <nnoskov@wiley.com>
2022-10-11 16:55:17 +0400

    added commandLineRunner for launching of scheduler

--- starter-test/src/main/java/com/sandbox/startertest/StarterTest.java
+++ starter-test/src/main/java/com/sandbox/startertest/StarterTest.java
@@ -1,15 +1,12 @@
 package com.sandbox.startertest;
 
-import com.sandbox.startertest.service.StarterTestService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StarterTest {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(StarterTest.class, args);
-        run.getBean(StarterTestService.class).runTask();
+        SpringApplication.run(StarterTest.class, args);
     }
 }
--- starter-test/src/main/java/com/sandbox/startertest/service/CommandLineRunnerImpl.java
+++ starter-test/src/main/java/com/sandbox/startertest/service/CommandLineRunnerImpl.java
@@ -1,22 +1,25 @@
 package com.sandbox.startertest.service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
-public class StarterTestService {
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
 
     private ThreadPoolTaskScheduler scheduler;
     private CronTrigger cronTrigger;
 
-    public StarterTestService(ThreadPoolTaskScheduler scheduler, CronTrigger cronTrigger) {
+    public CommandLineRunnerImpl(ThreadPoolTaskScheduler scheduler, CronTrigger cronTrigger) {
         this.scheduler = scheduler;
         this.cronTrigger = cronTrigger;
     }
 
-    public void runTask() {
+    @Override
+    public void run(String... args) throws Exception {
         scheduler.schedule(() -> System.out.println(Instant.now() + " I am working"), cronTrigger);
     }
 }
